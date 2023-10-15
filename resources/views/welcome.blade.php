b8edb643d6c433523bec32fa79cd0deb14e4220c
Aldrin Caballero <caballeroaldrin02@gmail.com>
2023-10-15 19:48:34 +0800

    Updated hahaha

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,6 +25,8 @@
 
                         {{-- If user login --}}
                         <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+
+                        
                     @else
 
                         {{-- if not login yet --}}
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
